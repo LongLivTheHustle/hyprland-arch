bd52f573a70e493e9faea7c2d6ad133f42b10f29
LLTH <khalil.taylor91@gmail.com>
2025-03-01 01:17:41 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-yay -S --noconfirm bottles calf cpupower-gui easyeffects-git fl-studio-integrator lsp-plugins mda.lv2 prismlauncher umu-launcher zam-plugins materialgram-bin wine-pure-git zen-browser-bin steam gamescope wine-gecko wine-mono wine-nine vkd3d lib32-vkd3d dxvk-bin winetricks
+yay -S --noconfirm bottles calf cpupower-gui easyeffects-git fl-studio-integrator lsp-plugins mda.lv2 prismlauncher gamemode lib32-gamemode umu-launcher zam-plugins materialgram-bin wine-pure-git zen-browser-bin steam gamescope wine-gecko wine-mono wine-nine vkd3d lib32-vkd3d dxvk-bin winetricks
 
  flatpak install --user https://sober.vinegarhq.org/sober.flatpakref 
  flatpak install --user org.freedesktop.Platform.VulkanLayer.gamescope/x86_64/23.08
